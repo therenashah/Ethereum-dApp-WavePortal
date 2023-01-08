e0415dba0af49a4efdfeb934fa67bd1bb7c10bee
Rena <113020538+therenashah@users.noreply.github.com>
2023-01-08 17:20:10 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
-# Ethereum-dApp-WavePortal
-A website that let's anyone wave at me after connecting their wallet and have that data saved on the blockchain through an Ethereum smart contract. 
-To make this a little spicy as well, when someone waves at  there is a random chance they can win a small amount of ETH from my site :)
-✅ Deployed a custom smart contract.
-✅ Built a "Connect Wallet" flow.
-✅ Created my own version of Web3 Twitter.
-✅ Designed UI from scratch using Figma.
+# Sample Hardhat Project
+
+This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
+
+Try running some of the following tasks:
+
+```shell
+npx hardhat help
+npx hardhat test
+REPORT_GAS=true npx hardhat test
+npx hardhat node
+npx hardhat run scripts/deploy.js
+```
---
+++ src/App.jsx
@@ -0,0 +1,177 @@
+
+import { ethers} from "ethers";
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import abi from "./utils/waveportal.json";
+
+const getEthereumObject = () => window.ethereum;
+
+/*
+ * This function returns the first linked account found.
+ * If there is no account linked, it will return null.
+ */
+const findMetaMaskAccount = async () => {
+  try {
+    const ethereum = getEthereumObject();
+
+    /*
+     * First make sure we have access to the Ethereum object.
+     */
+    if (!ethereum) {
+      console.error("Make sure you have Metamask!");
+      return null;
+    }
+
+    console.log("We have the Ethereum object", ethereum);
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account:", account);
+      return account;
+    } else {
+      console.error("No authorized account found");
+      return null;
+    }
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+const App = () => {
+  const [currentAccount, setCurrentAccount] = useState("");
+  const [allWaves, setAllWaves] = useState([]);
+  const contractAddress = "0x6BC35F45ea2967dCD32165A27d971F843BF99b6a";
+  const contractABI = abi.abi;
+  const getAllWaves = async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        /*
+         * Call the getAllWaves method from your Smart Contract
+         */
+        const waves = await wavePortalContract.getAllWaves();
+
+
+        /*
+         * We only need address, timestamp, and message in our UI so let's
+         * pick those out
+         */
+        let wavesCleaned = [];
+        waves.forEach(wave => {
+          wavesCleaned.push({
+            address: wave.waver,
+            timestamp: new Date(wave.timestamp * 1000),
+            message: wave.message
+          });
+        });
+
+        /*
+         * Store our data in React State
+         */
+        setAllWaves(wavesCleaned);
+        wavePortalContract.on("NewWave", (from, timestamp, message) => {
+          console.log("NewWave", from, timestamp, message);
+
+          setAllWaves(prevState => [...prevState, {
+            address: from,
+            timestamp: new Date(timestamp * 1000),
+            message: message
+          }]);
+        });
+      } else {
+        console.log("Ethereum object doesn't exist!")
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  const connectWallet = async () => {
+    try {
+      const ethereum = getEthereumObject();
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const wave = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+      
+        let count = await wavePortalContract.getTotalWaves();
+        console.log("Retrieved total wave count...", count.toNumber());
+      const waveTxn = await wavePortalContract.wave("this is a message", { gasLimit :300000});
+        console.log("Mining...", waveTxn.hash);
+
+        await waveTxn.wait();
+        console.log("Mined -- ", waveTxn.hash);
+
+        count = await wavePortalContract.getTotalWaves();
+        console.log("Retrieved total wave count...", count.toNumber());
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  /*
+   * This runs our function when the page loads.
+   * More technically, when the App component "mounts".
+   */
+  useEffect(async () => {
+    const account = await findMetaMaskAccount();
+    if (account !== null) {
+      setCurrentAccount(account);
+    }
+  }, [])
+  
+  return (
+  <html><body><div class="v5_2"><div class="v2_7"><div class="v2_3">Hey There!</div><div class="v2_4">I am Rena, this is my first dApp, so why don’t
+ you connect your ethereum wallet and wave at me</div>
+ {currentAccount && (
+    <button class="v28_7" onClick={wave}> 🌊 Wave at Rena</button>)}
+
+  {/*
+         * If there is no currentAccount render this button
+         */}
+        {!currentAccount && (
+          <button className="v28_7" onClick={connectWallet}>
+            🦊 Connect Wallet
+          </button>
+        )}
+     {allWaves.map((wave, index) => {
+          return (
+            <div style={{ backgroundColor: "OldLace", marginTop: "16px", padding: "8px" }}>
+              <div>Address: {wave.address}</div>
+              <div>Time: {wave.timestamp.toString()}</div>
+              <div>Message: {wave.message}</div>
+            </div>)
+        })}
+    </div></div></body></html>
+    
+     );
+}
+    
+export default App;
